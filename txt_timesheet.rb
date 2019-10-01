94fa5325d4d49a9ab09bac41e543c0211b443028
Andreluizsgf <Andreluizsgf@gmail.com>
2019-10-01 08:34:59 -0300

    fix problems

--- txt_timesheet.rb
+++ txt_timesheet.rb
@@ -1,58 +1,60 @@
 require 'time'
 
-file = IO.readlines("Timesheet.txt")
-
-morning, brb, back, leaving, finalTime = 0
-
-def get_Lines(file, string)
-  file.each { |l|
-    morning, brb, back, leaving = 0
-  }
-end
-
 def valid_Time? string
-  Time.parse(string) rescue nil
+    Time.parse(string) rescue nil
 end
 
-file.each { |l|
-  if(l.match?(/morning: (.*)/))
-    morning = l.sub(/.*?:/, '')
-  elsif(l.match?(/brb lunch: (.*)/))
-    brb = l.sub(/.*?:/, '')
-  elsif(l.match?(/back: (.*)/))
-    back = l.sub(/.*?:/, '')
-  elsif(l.match?(/leaving: (.*)/))
-    leaving = l.sub(/.*?:/, '')
-  end
+def toString array
+    array = array.join("\n")
+    array = array.split("\n")
+end
+
+file = File.open("Timesheet.txt")
+f_lines = file.read.split("## Timesheet\n")
+f_lines = f_lines[1].split("\n\n")
+f_lines = f_lines[0].split("\n")
+
+f_titles = Array.new(4) 
+f_hours = Array.new(4)
+i = 0
+
+f_lines.each { |l|
+    titles, *hours = l.split(/: /)
+    f_titles[i] = titles
+    f_hours[i] = hours    
+    i = i+1
 }
 
-if(valid_Time?(brb))
-  brb = Time.parse(brb).to_f
-  finalTime = brb
-else
-  puts "Could not parse 'brb lunch', so 'back' was disregarded" 
-end
+f_titles = toString(f_titles)
+f_hours = toString(f_hours)
 
-if(valid_Time?(morning))
-  morning = Time.parse(morning).to_f
-  finalTime -= morning
-else
-  finalTime = 0
-  puts "Could not parse 'morning', so 'brb lunch' was disregarded" 
-end
+if(valid_Time?(f_hours[2]))
+    brb = Time.parse(f_hours[2]).to_f
+    finalTime = brb
+  else
+    puts "Could not parse 'brb lunch', so 'back' was disregarded" 
+  end
 
-if(valid_Time?(leaving))
-  leaving = Time.parse(leaving).to_f
-  finalTime += leaving
-else
-  puts "Could not parse 'leaving'" 
+if(valid_Time?(f_hours[1]))
+    morning = Time.parse(f_hours[1]).to_f
+    finalTime -= morning
+  else
+    finalTime = 0
+    puts "Could not parse " + f_titles[1] + " , so " + f_titles[2] + "was disregarded"
 end
+  
+  if(valid_Time?(f_hours[4]))
+    leaving = Time.parse(f_hours[4]).to_f
+    finalTime += leaving
+  else
+    puts "Could not parse 'leaving'" 
+  end
+  
+  if(valid_Time?(f_hours[3]))
+    back = Time.parse(f_hours[3]).to_f
+    finalTime -= back
+  else
+    puts "Could not parse 'back', so 'leaving' was disregarded" 
+  end
 
-if(valid_Time?(back))
-  back = Time.parse(back).to_f
-  finalTime -= back
-else
-  puts "Could not parse 'back', so 'leaving' was disregarded" 
-end
-
-puts "Total hours: " + Time.at(finalTime).utc.strftime("%H:%M")
+  puts "Total hours: " + Time.at(finalTime).utc.strftime("%H:%M")
